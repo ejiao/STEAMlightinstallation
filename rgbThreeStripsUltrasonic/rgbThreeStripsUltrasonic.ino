2b490631591ad92dd066de3a9e12cd5fd857edff
Brad Guesman <james_guesman@brown.edu>
2017-04-03 23:50:43 -0400

    WORKS

--- rgbThreeStripsUltrasonic/rgbThreeStripsUltrasonic.ino
+++ rgbThreeStripsUltrasonic/rgbThreeStripsUltrasonic.ino
@@ -1,17 +1,17 @@
-#define ECHO1 22
-#define TRIG1 24
+#define ECHO1 40
+#define TRIG1 42
 
-#define ECHO2 30
-#define TRIG2 32
+#define ECHO2 22
+#define TRIG2 24
 
-#define ECHO3 40
-#define TRIG3 42
+#define ECHO3 30
+#define TRIG3 32
 
-#define AUDIO A0
+#define AUDIO 18
 
 #define RED1 3
-#define BLUE1 4
 #define GREEN1 2
+#define BLUE1 4
 
 #define RED2 7
 #define BLUE2 6
@@ -21,20 +21,22 @@
 #define BLUE3 10
 #define GREEN3 8
 
-int lastMode1 = 0;
-int lastMode2 = 0;
-
 static int FAR = 0,
            MEDIUM = 1,
            CLOSE = 2,
            STRIP_ONE = 1,
-           STRIP_TWO = 2, 
+           STRIP_TWO = 2,
            STRIP_THREE = 3,
-           R = 0, G = 1, B = 2; 
+           R = 0, G = 1, B = 2,
+           MAX_BRIGHTNESS = 255,
+           MED_BRIGHTNESS = 150,
+           IDLE_BRIGHTNESS = 50,
+           FRAME_RATE = 10;
 
-int oldStrip1[3], oldStrip2[3], oldStrip3[3],
-    newStrip1[3], newStrip2[3], newStrip3[3];
-
+int strip1[3], strip2[3], strip3[3];
+int count = 0;
+long distance1 = 151, distance2 = 151, distance3 = 151;
+unsigned long lastInterrupt;
 
 void setup() {
   // initialize pin modes
@@ -49,64 +51,162 @@ void setup() {
   pinMode(GREEN3, OUTPUT);
 
   // ultrasonic
-  pinMode(TRIG1, OUTPUT); 
+  pinMode(TRIG1, OUTPUT);
   pinMode(ECHO1, INPUT);
-   
+
   pinMode(TRIG2, OUTPUT);
   pinMode(ECHO2, INPUT);
-  
+
   pinMode(TRIG3, OUTPUT);
   pinMode(ECHO3, INPUT);
 
   Serial.begin(9600);
 
-  // initialize 
-  oldStrip1[R] = 200;
-  oldStrip2[G] = 200;
-  oldStrip3[B] = 200;
+  // add interrupt for audio
+  attachInterrupt(digitalPinToInterrupt(AUDIO), readAudio, RISING);
+
+  // initialize
+  strip1[R] = 0;
+  strip1[G] = 0;
+  strip1[B] = IDLE_BRIGHTNESS;
+
+  strip2[R] = 0;
+  strip2[G] = 0;
+  strip2[B] = IDLE_BRIGHTNESS;
+
+  strip3[R] = 0;
+  strip3[G] = 0;
+  strip3[B] = IDLE_BRIGHTNESS;
+
 
   // apply old state
-  rgb(oldStrip1, RED1, GREEN1, BLUE1);
-  rgb(oldStrip2, RED2, GREEN2, BLUE2);
-  rgb(oldStrip3, RED3, GREEN3, BLUE3);
-  
+  rgb(strip1, RED1, GREEN1, BLUE1);
+  rgb(strip2, RED2, GREEN2, BLUE2);
+  rgb(strip3, RED3, GREEN3, BLUE3);
 }
 
 void loop() {
-  // get distances from each ultrasonic sensor
-  long distance1 = ultrasonic(TRIG1, ECHO1); 
-  long distance2 = ultrasonic(TRIG2, ECHO2); 
-  long distance3 = ultrasonic(TRIG3, ECHO3); 
-  Serial.print("Strip one distance: "); 
-  Serial.println(distance1); 
-  Serial.print("Strip two distance: "); 
-  Serial.println(distance2); 
-  Serial.print("Strip three distance: ");
-  Serial.println(distance3);
+  if (count == 80) {
+    // get distances from each ultrasonic sensor
+    //    distance1 = ultrasonic(TRIG1, ECHO1);
+    distance2 = ultrasonic(TRIG2, ECHO2);
+    distance3 = ultrasonic(TRIG3, ECHO3);
+    //    Serial.print("Strip one distance: ");
+    //    Serial.println(distance1);
+    Serial.print("Strip two distance: ");
+    Serial.println(distance2);
+    Serial.print("Strip three distance: ");
+    Serial.println(distance3);
+    count = 0;
+  }
+
+  updateStripStates(distance1, distance2, distance3);
+
+  // apply new state
+  rgb(strip1, RED1, GREEN1, BLUE1);
+  rgb(strip2, RED2, GREEN2, BLUE2);
+  rgb(strip3, RED3, GREEN3, BLUE3);
+
+  //  Serial.print("Strip 3 Red: ");
+  //  Serial.println(strip3[R]);
+
+  delay(FRAME_RATE);
+
+  count++;
+
+}
+
+// updates all strip states
+void updateStripStates(int d1, int d2, int d3) {
+  updateParticularStrip(d1, R, strip1, 3);
+  updateParticularStrip(d2, R, strip2, 3);
+  updateParticularStrip(d3, R, strip3, 3);
+}
+
+// updates specific strip state
+void updateParticularStrip(int d, int color, int strip[], int inc) {
+  if (chunkDistance(d) == FAR) {
+    if (strip[color] > 0) {
+      //      Serial.println("DECREMENT THIS SHIT");
+      strip[color] -= inc;
+    }
+    else {
+      // do nothing
+    }
+    //    Serial.println("REACHED FAR");
+  }
+  else if (chunkDistance(d) == MEDIUM) {
+    if (strip[color] < MED_BRIGHTNESS) {
+      strip[color] += inc;
+    }
+    else if (strip1[color] > MED_BRIGHTNESS) {
+      strip[color] -= inc;
+    }
+    else {
+      // do nothing
+    }
+  }
+  else if (chunkDistance(d) == CLOSE) {
+    if (strip[color] < MAX_BRIGHTNESS - inc) {
+      strip[color] += inc;
+    }
+    else {
+      // do nothing
+    }
+  }
+}
+
+// decide how close something is to a sensor
+int chunkDistance(long d) { // zero distance may = far
+  if (d > 55) {
+    return FAR;
+  } else if (d > 30) {
+    return MEDIUM;
+  } else {
+    return CLOSE;
+  }
 }
 
 // Does the ultrasonic stuff
-// Modifies global "distances"  
-long ultrasonic(int trigPin, int echoPin) { 
-  long maxDuration = 0.;
-  
-  int trials = 5; 
-  for (int i = 0; i < trials; i++) { 
-      digitalWrite(trigPin, LOW);
-    
-      delayMicroseconds(20); 
-      
-      digitalWrite(trigPin, HIGH); 
-    
-      delayMicroseconds(100);
-    
-      long tempDuration = pulseIn(echoPin, HIGH);
+long ultrasonic(int trigPin, int echoPin) {
+  long minDuration = 50000.;
+  int trials = 3;
+  for (int i = 0; i < trials; i++) {
+    delayMicroseconds(2);
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(10);
 
-      if (tempDuration > maxDuration) {
-        maxDuration = tempDuration;
-      }
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(100);
+
+    long tempDuration = pulseIn(echoPin, HIGH);
+
+    if (tempDuration < minDuration) {
+      minDuration = tempDuration;
+    }
   }
-  return durationToDistance(maxDuration);
+  delayMicroseconds(200);
+  //  Serial.println(durationToDistance(minDuration));
+  return durationToDistance(minDuration);
+}
+
+// does the audio stuff
+void readAudio() {
+  if (millis() - lastInterrupt > 10) {
+    for (int i = 0; i < 255; i++) {
+      analogWrite(GREEN1, i);
+      analogWrite(GREEN2, i);
+      analogWrite(GREEN3, i);
+      delayMicroseconds(1000);
+    }
+    for (int i = 255; i >= 0; i--) {
+      analogWrite(GREEN1, i);
+      analogWrite(GREEN2, i);
+      analogWrite(GREEN3, i);
+      delayMicroseconds(1000);
+    } 
+  }
+  lastInterrupt = millis();
 }
 
 long durationToDistance(long duration) {
@@ -129,7 +229,10 @@ void fadeColor(int startR, int startG,
     int newR = startR + (i * incR);
     int newB = startB + (i * incB);
     int newG = startG + (i * incG);
-    rgb({newR, newB, newG}, pinRed, pinGreen, pinBlue);
+    int newRGB[] = {newR, newB, newG};
+    rgb(newRGB, pinRed, pinGreen, pinBlue);
+    rgb(newRGB, RED2, GREEN2, BLUE2);
+    rgb(newRGB, RED3, GREEN3, BLUE3);
     delay(pause);
   }
 }
